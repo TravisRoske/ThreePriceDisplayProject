a75ec51b3316e8a9030a707d5e16319657c55232
Flacid Papa <travisroske@gmail.com>
2022-09-30 00:35:21 -0700

    .

--- src/objects/HologramObject.js
+++ src/objects/HologramObject.js
@@ -1,48 +1,43 @@
 import { FontLoader } from '../myFontLoader.js'  //only exists locally in src file, not to be found elsewhere
 import { TextGeometry } from '../TextGeometry.js'
 
-        // const font2 = loader.load(
-        //     'font.json', 
-        
-        //     // onLoad callback
-        //     function ( font2 ) {
-
-        //         const btcGeometry = new TextGeometry( `Bitcoin: ${price}`, {
-        //             font: font2,
-        //             size: 250,
-        //             height: 70,
-        //             curveSegments: 12,
-        //             bevelEnabled: true,
-        //             bevelThickness: 10,
-        //             bevelSize: 8,
-        //             bevelOffset: 0,
-        //             bevelSegments: 5
-        //         } );
-        //         const btcMaterial = new THREE.MeshStandardMaterial()
-        //         btcMaterial.color = new THREE.Color(0x0000ff)
-
-        //         btcMaterial.transparent = true;
-        //         btcMaterial.opacity = .5
-
-
-        //         bitcoin.geometry = btcGeometry
-        //         bitcoin.material = btcMaterial
-        //     },
-        
-
-        //     // onError callback
-        //     function ( err ) {
-        //         console.log( 'An error happened' );
-        //     }
-        // )
 
 
 
 
+const loader = new FontLoader();
+const font = loader.load(
+    'font.json', //somehow this resolves to the static folder, with or without the ./
 
+	// onLoad callback
+	function ( font ) {
+		// do something with the font
+        const geometry = new TextGeometry( 'Fonts and static files have to be in the static folder...The path automatically resolves there...', {
+            font: font,
+            size: 250,
+            height: 70,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelOffset: 0,
+            bevelSegments: 5
+        } );
+        const material2 = new THREE.MeshStandardMaterial()
+        material2.color = new THREE.Color(0x00ff00)
 
+        // Mesh
+        const text = new THREE.Mesh(geometry,material2)
+        scene.add(text)
 
+        text.scale.set(.0025,.0025,.0025)
+	},
 
+	// onError callback
+	function ( err ) {
+		console.log( 'An error happened' );
+	}
+);
         
 
 
