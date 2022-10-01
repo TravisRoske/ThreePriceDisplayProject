f1ed1306973d3951ef33a33f728d579140024bca
Flacid Papa <travisroske@gmail.com>
2022-09-30 22:04:52 -0700

    Tile generation partly done

--- src/objects/FloorTileObject.js
+++ src/objects/FloorTileObject.js
@@ -1,2 +1,25 @@
 //These will spawn automatically and despawn based on the user's position
 //They will open up when a new console spawns underneath them...
+import * as THREE from 'three'
+
+
+export class FloorTileObject{
+    constructor(posX, posZ){
+        this.x = posX;
+        this.z = posZ;
+
+        this.geo = new THREE.PlaneGeometry(1,1)
+        this.material = new THREE.MeshStandardMaterial();
+
+        this.mesh = new THREE.Mesh(this.geo, this.material);
+
+
+        this.mesh.position.x =  this.x;
+        this.mesh.position.z =  this.z;
+
+        //temp/// will delete when i upload actual objects
+        this.mesh.rotation.x = - Math.PI / 2
+        ////
+
+    }
+}
--- src/script.js
+++ src/script.js
@@ -2,7 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-import {ConsoleObject} from './objects/ConsoleObject.js'
+import { ConsoleObject } from './objects/ConsoleObject.js'
+import { FloorTileObject } from './objects/FloorTileObject'
 
 
 
@@ -92,13 +93,15 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+
+//Initially generates the floor tiles around camera position
+let tiles = generateTiles(camera.position);
+
+
 /**
  * Animate
  */
-
-
 const clock = new THREE.Clock()
-
 
 const tick = () =>
 {
@@ -114,7 +117,18 @@ const tick = () =>
     // Render
     renderer.render(scene, camera)
 
+// /////////
+    tiles = updateTiles(camera.position, tiles)
+    console.log(tiles.length, tiles[0].length)
 
+
+    //get player position
+    //generate tiles around the player position...
+    //start with position... then load in a tile every space around the player...for 50 spaces each way
+    //a 2d array of tiles, when you move, pop or shift from one side
+    //then push or unshift new tiles....
+    //each tile has a position... denoting it's center???
+// //////////
 
 
     //Just put main loop in here...
@@ -144,3 +158,95 @@ scene.add(consoleTest.mesh)
 let consoleTest2 = new ConsoleObject();
 scene.add(consoleTest2.mesh)
 consoleTest2.mesh.position.z = -4
+
+
+
+function generateTiles(cameraPosition, tileGenerateDistance = 20){
+    let tiles = []
+
+    let originX = Math.floor(cameraPosition.x)
+    let originZ = Math.floor(cameraPosition.y)
+
+    for(let x = originX - 20; x < originX + 20; x++){
+
+        let zRow = []
+
+        for(let z = originZ - 20; z < originZ + 20; z++){
+
+            let tile = new FloorTileObject( x,z );
+
+            zRow.push(tile)
+
+            scene.add(tile.mesh)
+
+        }
+
+        tiles.push(zRow)
+    }
+
+    return tiles;
+}
+
+///Work more on this later...
+function updateTiles(cameraPosition, tiles, tileGenerateDistance = 20){
+
+    let originX = Math.floor(cameraPosition.x)
+    let originZ = Math.floor(cameraPosition.z)
+
+    let lowX = tiles[0][0].x;
+    let highX = tiles[tiles.length - 1][0].x;
+    let lowZ = tiles[0][0].z;
+    let highZ = tiles[0][tiles[0].length - 1].z;
+
+    //if you moved positive on the x axis
+    if(lowX - cameraPosition.x < -tileGenerateDistance){
+        for(let step = 0; lowX + step + tileGenerateDistance < originX; step++){ ///Doens't work right
+            //remove the first(lowest x) row of the tiles 2d array
+            for(let t of tiles[0]){
+                scene.remove(t.mesh)
+            }
+            tiles.shift()
+
+            //add a new row to the upper x value
+            let zRow = []
+            let x = Math.floor(cameraPosition.x)
+            let originZ = Math.floor(cameraPosition.z)
+
+            for(let z = originZ - tileGenerateDistance; z < originZ + tileGenerateDistance; z++){
+
+                let tile = new FloorTileObject( originX + step + tileGenerateDistance, z );
+
+                zRow.push(tile)
+
+                scene.add(tile.mesh)
+            }
+
+            tiles.push(zRow)
+        }
+    }
+
+    //if you moved negative on the x axis
+    if(highX - cameraPosition.x > tileGenerateDistance){
+        //remove the last (highest x) row of the tiles 2d array
+        for(let t of tiles[tiles.length - 1]){
+            scene.remove(t.mesh)
+        }
+        tiles.pop()
+
+        //add a new row to the lower x value
+        let zRow = []
+
+        for(let z = originZ - tileGenerateDistance; z < originZ + tileGenerateDistance; z++){
+
+            let tile = new FloorTileObject( originX - tileGenerateDistance , z );
+
+            zRow.push(tile)
+
+            scene.add(tile.mesh)
+        }
+
+        tiles.unshift(zRow)
+    }
+
+    return tiles;
+}
